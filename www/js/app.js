942714ca8ab242c3971d70987c1add0f53e313e5
Artem Demo <artemdemo@gmail.com>
2014-10-21 23:09:25 +0300

    home view

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,27 @@
 var todoBluefish = angular.module('todoBluefish', ['ionic'])
 
-.run(function($ionicPlatform) {
-	$ionicPlatform.ready(function() {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
-		if(window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		}
-		if(window.StatusBar) {
-			StatusBar.styleDefault();
-		}
-	});
-})
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.controller('mainCtrl', function( $scope, $timeout ){
-	
-});
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('home', {
+            url: '/',
+            templateUrl: 'pages/home.html'
+        })
+    })
+
+    .controller('mainCtrl', function( $scope, $timeout ){
+
+    });
